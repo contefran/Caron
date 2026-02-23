c31d6cf43a78166ed4068a757710ceb8d20e20e3
Francesco <contefrances@gmail.com>
2026-02-23 17:27:10 +0100

    merged from simulation and cleaned

--- caron/data.py
+++ caron/data.py
@@ -24,18 +24,12 @@ class Data:
     sim_cmd_version: int = 0
     sim_paused: bool = False
     sim_finished: bool=False
-<<<<<<< simulation
     coords: tuple = field(init = False, repr = False) # to be set later
-
-    def __post_init__(self) -> None:
-        self.buffer: deque = deque(maxlen=self.maxlen)
-=======
     
     def __post_init__(self) -> None:
         self.coords: tuple  # spatial coordinate -- to be set by simulation
         self.quantities: list[str] # for instance ['Density', 'Pressure', 'Velocity']. Pushed by simulation
-        self.buffer = deque(maxlen=self.maxlen)
->>>>>>> main
+        self.buffer: deque = deque(maxlen=self.maxlen)
         self.lock = threading.Lock() # to protect buffer access
         self.cond = threading.Condition(self.lock) # to notify when new frames are available
         self.buffer_safe_min=self.args.calib_frames
